3bb71545ada5b83e43fd5333ecbaf5b7f5a9b8d8
dev-DaviGuerra <davimguerra@gmail.com>
2025-10-21 11:44:52 -0300

    Paginacao Last

--- aeroporto_etl/aeroporto_etl/extrair.py
+++ aeroporto_etl/aeroporto_etl/extrair.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 import requests
 
@@ -17,9 +18,32 @@ def get_endpoint(endpoint, endpoint_id=None, params=None):
 
     if endpoint_id:
         url = url + "/" + endpoint_id
-    resultado_get = requests.get(url=url, headers=headers, params=None)
 
-    import ipdb; ipdb.set_trace()
+    resultado_get = requests.get(url=url, headers=headers, params=None)
+    
+    numero_paginas = processar_headers_numero_paginas(resultado_get.headers)
+
+
+def processar_headers_numero_paginas(headers):
+
+    headers_link = headers.get('link')
+    if not headers_link:
+        return '0'
+    
+    link_partes = headers_link.split(',')
+
+    exp_regular = r".*page=([0-9]+).*"
+
+    for link_parte in link_partes:
+        if 'rel="last"' in link_parte:
+            numero = link_parte.split(';')[0]
+            numero = re.search(exp_regular, numero)
+            
+            import ipdb; ipdb.set_trace()
+            return numero.groups()[0]
+            
+
+
 
 if __name__ == "__main__":
     get_endpoint('flights')
