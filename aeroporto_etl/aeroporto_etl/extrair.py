7ea5d08c1201be4eb94bce238e4b65ebed66aea7
dev-DaviGuerra <davimguerra@gmail.com>
2025-10-24 13:22:44 -0300

    Todos os GETs

--- aeroporto_etl/aeroporto_etl/extrair.py
+++ aeroporto_etl/aeroporto_etl/extrair.py
@@ -2,6 +2,9 @@ import os
 import re
 import logging
 import time
+import pytz
+from datetime import datetime
+from datetime import timedelta
 from pathlib import Path
 import requests
 
@@ -37,7 +40,7 @@ def get_endpoint(endpoint, endpoint_id=None, params=None):
 
     resultados = [] 
 
-    resultado_get = requests.get(url=url, headers=headers, params=None)
+    resultado_get = requests.get(url=url, headers=headers)
     resultado_get.raise_for_status()
 
     resultados.append(resultado_get.json())
@@ -47,7 +50,7 @@ def get_endpoint(endpoint, endpoint_id=None, params=None):
     while link:=processar_headers_next(resultado_get.headers):
         time.sleep(0.5)
         logger.info(f"Páginas: {numero_paginas}, link: {link}")
-        resultado_get = requests.get(url=link, headers=headers, params=None)
+        resultado_get = requests.get(url=link, headers=headers)
         resultado_get.raise_for_status()
         resultados.append(resultado_get.json())
 
@@ -79,7 +82,7 @@ def processar_headers_numero_paginas(headers):
     if not headers_link:
         return '0'
     
-    link_partes = headers_link.split(',')
+    link_partes  = headers_link.split(',')
 
     exp_regular = r".*page=([0-9]+).*"
 
@@ -89,9 +92,46 @@ def processar_headers_numero_paginas(headers):
             numero = re.search(exp_regular, numero)
             
             return numero.groups()[0]
-            
+    return '0'
+
+
+def get_flights_agendado_hoje():
+    return get_endpoint('flights')
+
+def get_flights_agendado_ontem():
+
+    agora = datetime.now(pytz.timezone('Europe/Amsterdam'))
+    ontem = agora.date() - timedelta(days=1)
+    
+    ontem = ontem.strftime("%Y-%m-%d")
+
+    params = {
+        "scheduleDate": ontem
+
+    }
+    print(params)
+    return get_endpoint(endpoint='flights', params=params)            
+
+def get_flight_por_id(flight_id):
+    return get_endpoint(endpoint='flights', endpoint_id=flight_id)
+
+def get_airlines():
+    return get_endpoint(endpoint='airlines')
+
+def get_airline_por_iata_icao(iata_ou_icao):
+    return get_endpoint(endpoint='airlines', endpoint_id=iata_ou_icao)
+
+def get_aircraft_types():
+    return get_endpoint(endpoint='aircrafttypes')
+
+def get_destinations():
+    return get_endpoint(endpoint='destinations')
+
+def get_destination_por_iata(iata):
+    return get_endpoint(endpoint='destinations', endpoint_id=iata)
 
 
 
 if __name__ == "__main__":
-    get_endpoint('flights')
+    resultado = get_destination_por_iata('AAB')
+    print(resultado)
