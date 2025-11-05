d14a0800ff323cd8e411bd0b90929b1bc7aebfd5
dev-DaviGuerra <davimguerra@gmail.com>
2025-11-05 14:34:32 -0300

    ETL finalizada

--- aeroporto_etl/aeroporto_etl/main.py
+++ aeroporto_etl/aeroporto_etl/main.py
@@ -1,24 +1,33 @@
-from extrair import get_airlines, get_aircraft_types, get_destinations
+from extrair import get_airlines, get_aircraft_types, get_destinations, get_flights_agendado_hoje
 from salvar import salvar
-from transformar import transformar_airlines, transformar_aircraft_types, transformar_destinations
+from transformar import transformar_airlines, transformar_aircraft_types, transformar_destinations, transformar_flights
 
 
 def main_etl():
-    ##airlines_paginas = get_airlines()
-    ##aircraft_types_paginas = get_aircraft_types()
+    flights_paginas = get_flights_agendado_hoje()
+    airlines_paginas = get_airlines()
+    aircraft_types_paginas = get_aircraft_types()
     destinations_paginas = get_destinations()
 
-    ##airlines = transformar_airlines(airlines_paginas)
-    ##aircraft_types = transformar_aircraft_types(aircraft_types_paginas)
+    flights = transformar_flights(flights_paginas)
+    airlines = transformar_airlines(airlines_paginas)
+    aircraft_types = transformar_aircraft_types(aircraft_types_paginas)
     destinations = transformar_destinations(destinations_paginas)
 
     salvar(
         '../',
-        [
+        [   
+            flights,
+            airlines,
+            aircraft_types,
             destinations,
         ],
         [
-            'destinations'
+            'flights',
+            'airlines',
+            'aircraft_types',
+            'destinations',
+            
         ]
     )
 if __name__ == "__main__":
--- aeroporto_etl/aeroporto_etl/transformar.py
+++ aeroporto_etl/aeroporto_etl/transformar.py
@@ -1,3 +1,130 @@
+from datetime import datetime, UTC, timezone
+
+def atributos_data_para_dict(flight, lista_atributos):
+
+    resultados = {}
+
+    for atributo in lista_atributos:
+        data = flight.get(atributo)
+        
+        if data:
+            try:
+                data = datetime.fromisoformat(data).astimezone(timezone.utc)
+            except ValueError:
+                data = None
+        
+        resultados[atributo] = data
+
+    return resultados
+
+
+
+def atributos_para_dict(flight, lista_atributos):
+    resultados = dict()
+
+    for atributo in lista_atributos:
+        resultados[atributo] = flight.get(atributo)
+
+    return resultados
+
+def transformar_flights(flights_paginas):
+
+    resultados = []
+    flights = []
+
+    for flight_pagina in flights_paginas:
+        flights.extend(flight_pagina.get('flights'))
+
+    for flight in flights:
+        aircraftType = flight.get('aircraftType')
+
+        aircraftType_iataMain = None
+        aircraftType_iataSub = None
+        if aircraftType:
+            aircraftType_iataMain = aircraftType.get('iataMain')
+            aircraftType_iataSub = aircraftType.get('iataSub')
+
+        route = None
+        eu = None
+        visa = None
+        flight_route = flight.get('route')
+        if flight_route:
+            destinations = flight_route.get('destinations')
+            eu = flight_route.get('eu')
+            visa = flight_route.get('visa')
+            if destinations:
+                route = ",".join(destinations)
+
+        codeshares = None
+        flight_codeshares = flight.get('codeshares')
+        if flight_codeshares:
+            flight_codeshares = flight.get('codeshares')
+            if flight_codeshares:
+                codeshares = ",".join(flight_codeshares)
+
+        flight_states = None
+        public_flight_states = flight.get('publicFlightState')
+        if public_flight_states:
+            flight_states = public_flight_states.get('flightStates')
+            if flight_states:
+                flight_states = ",".join(flight_states)
+
+        atributos = {
+            'aircraftType_iataMain': aircraftType_iataMain,
+            'aircraftType_iataSub': aircraftType_iataSub,
+            'route': route,
+            'codeshares': codeshares,
+            'flightStates': flight_states,
+            'eu': eu,
+            'visa': visa
+        }
+
+        atributos.update(
+            atributos_para_dict(
+                flight,
+                [
+                    'flightDirection',
+                    'flightName',
+                    'flightNumber',
+                    'gate',
+                    'pier',
+                    'id',
+                    'isOperationalFlight',
+                    'mainFlight',
+                    'prefixIATA',
+                    'prefixICAO',
+                    'airlineCode',
+                    'aircraftRegistration',
+                    'serviceType',
+                    'terminal',
+                ]
+            )
+        )
+
+        atributos.update(
+            atributos_data_para_dict(
+                flight,
+                [
+                    'estimatedLandingTime',
+                    'lastUpdatedAt',
+                    'actualLandingTime',
+                    'scheduleDateTime',
+                    'actualOffBlockTime',
+                    'expectedTimeBoarding',
+                    'expectedTimeGateClosing',
+                    'expectedTimeGateOpen',
+                    'expectedTimeOnBelt',
+                    'expectedSecurityFilter',
+                    'publicEstimatedOffBlockTime',
+                    
+                ]
+            )
+        )
+
+        resultados.append(atributos)
+
+    return resultados
+
 def transformar_airlines(airlines_paginas):
 
     resultados = []
